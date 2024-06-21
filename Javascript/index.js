018c10576228acfd042c2c23e39f839d4b6d5467
Henrik Brüsecke <henrik@instantworkstation.com>
2024-06-21 11:10:17 +0300

    Added Javascript benchmark

---
+++ Javascript/index.js
@@ -0,0 +1,10 @@
+const crypto = require('crypto');
+const randomstring = require("randomstring");
+
+const data = randomstring.generate(1024 * 1024);
+
+const executionStart = performance.now();
+const hash = crypto.createHash('sha256').update(data);
+const executionEnd = performance.now();
+
+console.log("SHA-256 speed: " + Math.round(1000 / (executionEnd - executionStart)) + " MB/s");
