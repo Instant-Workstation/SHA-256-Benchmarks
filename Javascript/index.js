20de420826683d135dfb18952ddd032e2a38bac6
Henrik Brüsecke <henrik@instantworkstation.com>
2024-06-29 15:50:48 +0300

    Hash exactly 25MB

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,12 +1,18 @@
 #! /usr/bin/env node
 
 const crypto = require('crypto');
-const randomstring = require("randomstring");
 
-const data = randomstring.generate(1024 * 1024 * 10);
+const blockSize = 65536;
+const data = new Uint8Array(1024 * 1024 * 25);
+
+for (let i = 0; i < data.length; i += blockSize) {
+    var dataBlock = new Uint8Array(blockSize);
+    crypto.getRandomValues(dataBlock);
+    data.set(dataBlock, i);
+}
 
 const executionStart = performance.now();
 const hash = crypto.createHash('sha256').update(data);
 const executionEnd = performance.now();
 
-console.log("SHA-256 speed: " + Math.round((1000 / (executionEnd - executionStart)) * 10) + " MB/s");
+console.log("SHA-256 speed: " + Math.round((1000 / (executionEnd - executionStart)) * 25) + " MB/s");
