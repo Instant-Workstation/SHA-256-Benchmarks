797c6e1f8e08468c03f8e2fa30f3ae449ff8bd05
Henrik Brüsecke <henrik@instantworkstation.com>
2024-06-29 15:11:25 +0300

    Improved accuracy of JavaScript benchmark

--- Javascript/index.js
+++ Javascript/index.js
@@ -3,10 +3,10 @@
 const crypto = require('crypto');
 const randomstring = require("randomstring");
 
-const data = randomstring.generate(1024 * 1024);
+const data = randomstring.generate(1024 * 1024 * 10);
 
 const executionStart = performance.now();
 const hash = crypto.createHash('sha256').update(data);
 const executionEnd = performance.now();
 
-console.log("SHA-256 speed: " + Math.round(1000 / (executionEnd - executionStart)) + " MB/s");
+console.log("SHA-256 speed: " + Math.round((1000 / (executionEnd - executionStart)) * 10) + " MB/s");
